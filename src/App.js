cb0714dada65b467da912acc18ad5544debf556a
DESKTOP-31QJCO2\shokwave <tim.ku2004@gmail.com>
2023-01-20 18:12:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Form } from "./components/Form";
+import { Todos } from "./components/Todos";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteAll } from "./redux/todoapp/actions";
 
 function App() {
+  // dispatch function to dispatch an action
+  const dispatch = useDispatch();
+
+  // getting todos state for conditional rendering
+  const todos = useSelector((state) => state.operationsReducer);
+
+  // update form visibility state
+  const [editFormVisibility, setEditFormVisibility] = useState(false);
+
+  // editTodo state
+  const [editTodo, setEditTodo] = useState("");
+
+  // this function will trigger when someone clicks the edit icon
+  const handleEditClick = (todo) => {
+    setEditFormVisibility(true);
+    setEditTodo(todo);
+  };
+
+  // back button click
+  const cancelUpdate = () => {
+    setEditFormVisibility(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="wrapper">
+      <br></br>
+      <h1 className="text-center">ToDoList with React-Redux</h1>
+      <Form
+        editFormVisibility={editFormVisibility}
+        editTodo={editTodo}
+        cancelUpdate={cancelUpdate}
+      />
+      <Todos
+        handleEditClick={handleEditClick}
+        editFormVisibility={editFormVisibility}
+      />
+      {todos.length > 1 && (
+        <button
+          className="btn btn-danger btn-md delete-all"
+          onClick={() => dispatch(deleteAll())}
         >
-          Learn React
-        </a>
-      </header>
+          DELETE ALL
+        </button>
+      )}
     </div>
   );
 }
